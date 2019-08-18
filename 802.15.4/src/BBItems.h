caa8115e70a0b7622f9eedbc4b10404e49bb526d
mariax87 <mariax87@hotmail.com>
2019-08-18 12:19:25 +0300

    Update BBItems.h

--- 802.15.4/src/BBItems.h
+++ 802.15.4/src/BBItems.h
@@ -1,16 +1,5 @@
 /* -*- mode:c++ -*- ********************************************************
  * file:        BBItems.h
- *
- * author:      Bernard Halas
- *
- * copyright:   (C) 2008 Czech Technical University, Faculty of
- *              Electrical Engineering
- *
- *              This program is free software; you can redistribute it
- *              and/or modify it under the terms of the GNU General Public
- *              License as published by the Free Software Foundation; either
- *              version 2 of the License, or (at your option) any later
- *              version.
  ***************************************************************************
  * part of:     ZigBee Simulation model
  * description: - Blackboard objects containing radio state values
