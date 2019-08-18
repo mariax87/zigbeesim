87c383bd37e3a5dc2e8165f9a821b63468bda99e
mariax87 <mariax87@hotmail.com>
2019-08-18 11:57:49 +0300

    Update FFDAppLayer.h

--- dip_802.15.4/src/FFDAppLayer.h
+++ dip_802.15.4/src/FFDAppLayer.h
@@ -1,18 +1,4 @@
-/* -*- mode:c++ -*- ********************************************************
- * file:        BasicApplLayer.h
- *
- * author:      Daniel Willkomm
- *
- * copyright:   (C) 2004 Telecommunication Networks Group (TKN) at
- *              Technische Universitaet Berlin, Germany.
- *
- *              This program is free software; you can redistribute it
- *              and/or modify it under the terms of the GNU General Public
- *              License as published by the Free Software Foundation; either
- *              version 2 of the License, or (at your option) any later
- *              version.
- *              For further information see file COPYING
- *              in the top level directory
+/* 
  ***************************************************************************
  * part of:     framework implementation developed by tkn
  * description: application layer: general class for the application layer
@@ -38,7 +24,6 @@
  *
  * @ingroup applLayer
  *
- * @author Daniel Willkomm
  **/
 class FFDAppLayer: public BasicApplLayer {
 public:
