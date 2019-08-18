dd4401a6d95e9780e7d3b08885f5a9019d895de1
mariax87 <mariax87@hotmail.com>
2019-08-18 11:54:56 +0300

    Update BBInt.h

--- dip_802.15.4/src/BBInt.h
+++ dip_802.15.4/src/BBInt.h
@@ -1,17 +1,5 @@
-/* -*- mode:c++ -*- ********************************************************
- * file:        BBInt.h
- *
- * author:      Bernard Halas
- *
- * copyright:   (C) 2006 Czech Technical University, Faculty of
- *              Electrical Engineering
- *
- *              This program is free software; you can redistribute it 
- *              and/or modify it under the terms of the GNU General Public 
- *              License as published by the Free Software Foundation; either
- *              version 2 of the License, or (at your option) any later 
- *              version.
- ***************************************************************************
+
+ /***************************************************************************
  * part of:     ZigBee Simulating program
  * description: - Blackboard structure for publishing integer values
  ***************************************************************************/
