22fcb76e8de68ea8c6e6b0973ab8b99f45b7a738
mariax87 <mariax87@hotmail.com>
2019-08-18 12:01:41 +0300

    Update Mac802154.cc

--- dip_802.15.4/src/Mac802154.cc
+++ dip_802.15.4/src/Mac802154.cc
@@ -1,19 +1,6 @@
 /* -*- mode:c++ -*- ********************************************************
  * file:        CSMAMacLayer.cc
  *
- * author:      Marc Loebbers, Yosia Hadisusanto, Andreas Koepke
- *
- * copyright:   (C) 2004,2005,2006
- *              Telecommunication Networks Group (TKN) at Technische
- *              Universitaet Berlin, Germany.
- *
- *              This program is free software; you can redistribute it
- *              and/or modify it under the terms of the GNU General Public
- *              License as published by the Free Software Foundation; either
- *              version 2 of the License, or (at your option) any later
- *              version.
- *              For further information see file COPYING
- *              in the top level directory
  ***************************************************************************
  * part of:     framework implementation developed by tkn
  ***************************************************************************/
